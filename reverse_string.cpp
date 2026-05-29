0fb35dbdaae4d07875232b42bf678744ff26d9d6
ArjavJain1708 <i.arjavjain@gmail.com>
2026-05-29 13:09:32 +0530

    this is my first commit

---
+++ reverse_string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char a;
+      for(int i=0;i<s.size()/2 ;i++){
+        a=s[i];
+         s[i]=s[s.size()-1-i];
+         s[s.size()-1-i]=a;
+      }  
+    }
+};
