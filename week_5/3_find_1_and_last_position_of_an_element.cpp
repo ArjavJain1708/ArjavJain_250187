b73eb1d194842fc62218aaf57ee66adb64a31e6f
ArjavJain1708 <i.arjavjain@gmail.com>
2026-06-30 20:26:20 +0530

    sixth commit

---
+++ week_5/3_find_1_and_last_position_of_an_element.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int a=-2;
+        int b=0;
+        int mid,high=nums.size()-1,low=0;
+        vector<int> ans;
+        
+        while(low<=high){
+mid=low+((high-low)/2);
+if(nums[mid]>target){
+    high=mid-1;
+}
+
+else if(nums[mid]<target){
+  low=mid+1;
+}
+
+else{
+    a=mid;
+    b=mid;
+   while(a>=0&&b<nums.size()&&((nums[a]==target)&&(nums[b]==target)))
+   {
+    a--;
+    b++;
+   }
+   break;
+}
+}
+
+
+while (a >= 0 && nums[a] == target) {
+    a--;
+}
+
+
+while (b < nums.size() && nums[b] == target) {
+    b++;
+}
+
+ans.push_back(a+1);
+ans.push_back(b-1);
+ 
+  return ans;  }
+};
